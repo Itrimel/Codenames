c77c55cd0e72945861e1fae0d30b9f38d747ed19
Itrimel <void@void.c>
2020-03-08 15:26:26 +0100

    Premiers fichiers

---
+++ qcard.cpp
@@ -0,0 +1,6 @@
+#include "qcard.h"
+
+QCard::QCard(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ qcard.h
@@ -0,0 +1,16 @@
+#ifndef QCARD_H
+#define QCARD_H
+
+#include <QWidget>
+
+class QCard : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QCard(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // QCARD_H
